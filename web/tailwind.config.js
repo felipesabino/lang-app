a14c0bc447175c2312293b59221e7384080cb85b
Felipe Sabino <felipe@sabino.me>
2023-05-16 08:47:02 -0300

    added waiting room style

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -6,6 +6,36 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    extend: {
+      animation: {
+        "waiting-room": "waiting-room 14s infinite"
+      },
+      keyframes: {
+        "waiting-room": {
+          "12.5%, 22.5%": {
+              transform: 'translateY(-12.5%)'
+          },
+          "25%, 35%": {
+              transform: 'translateY(-25%)'
+          },
+          "37.5%, 47.5%": {
+              transform: 'translateY(-37.5%)'
+          },
+          "50%, 60%": {
+              transform: 'translateY(-50%)'
+          },
+          "62.5%, 72.5%": {
+              transform: 'translateY(-62.5%)'
+          },
+          "75%, 85%": {
+              transform: 'translateY(-75%)'
+          },
+          "87.5%, 100%": {
+              transform: 'translateY(-87.5%)'
+          },
+        }
+      }
+    }
   },
   plugins: [],
 }
